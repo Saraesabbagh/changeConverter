d384d6942ddf6e93a1bed829c0b7e0375928fdf3
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-14 11:47:23 +0100

    ended session at 3 tests

--- changeConverter2.js
+++ changeConverter2.js
@@ -1,9 +1,21 @@
 const changeConverter2 = (pounds_number) => {
-    if (pounds_number == 50.00) {
-        return ['£50'];
-    } else if (pounds_number == 100.00) {
-        return ['£50', '£50'];
-    };
+    const pound_notes = [50.00, 20.00, 10.00, 5.00]
+    const change = []
+    if (pound_notes.includes(pounds_number)) {
+        change.push("£"+Math.floor(pounds_number))
+        return change;
+    } else {
+        for (let i = 0; i < pound_notes.length; i++) {
+            // output is a number - float
+            const num = pounds_number / pound_notes[i]
+
+            if (Number.isInteger(num)) {
+                
+                change.push(("£"+pound_notes[i])* num) 
+            }
+        }
+        return change
+    }
         
 }
 
--- changeConverter2.test.js
+++ changeConverter2.test.js
@@ -7,4 +7,8 @@ describe('changeConverter2', () => {
   it('returns 2 £50s when given exact number 100.00', ()=>{
     expect(changeConverter2(100.00)).toEqual(["£50", "£50"]);
   });
+  it("returns 1 £20 and 1 £50 when given exact number 70.00", () => {
+    expect(changeConverter2(70.00)).toEqual(["£50","£20"]);
+  });
+  
 })
